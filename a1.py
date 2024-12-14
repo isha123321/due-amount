2a14d1c65bfbd8e06e0fe996663a57a0ab44b116
isha123321 <isha101112@hotmail.com>
2024-12-14 17:03:18 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,4 @@
+total_bill=float(input("enter the total bill amount:"))
+amount_paid=float(input("enter the amount paid by the costumer:"))
+due_amount=total_bill - amount_paid
+print("the due amount by the costumer is :", due_amount)
